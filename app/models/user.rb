dd4a10ff662339d3f7f8efe80b205e0d53795bd2
maybesan <seanbourkemckenna@gmail.com>
2015-05-01 19:34:59 +0100

    Added password reset option for login form and the functionality.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
 before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
@@ -9,6 +9,11 @@ before_create :create_activation_digest
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }, allow_blank: true
+
+  # Returns true if a password reset has expired.
+def password_reset_expired?
+  reset_sent_at < 2.hours.ago
+end
 
   # Returns the hash digest of the given string.
   def User.digest(string)
@@ -61,4 +66,16 @@ before_create :create_activation_digest
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
+
+  # Sets the password reset attributes.
+ def create_reset_digest
+   self.reset_token = User.new_token
+   update_attribute(:reset_digest,  User.digest(reset_token))
+   update_attribute(:reset_sent_at, Time.zone.now)
+ end
+
+ # Sends password reset email.
+ def send_password_reset_email
+   UserMailer.password_reset(self).deliver_now
+ end
 end
